0cb58e8682952d7520b8823c4635cdd26d4e5358
visheshkumar9648 <142682585+visheshkumar9648@users.noreply.github.com>
2024-04-07 11:18:09 +0530

    Add files via upload

---
+++ ecommerce/loginsignup/ContactUs/ContactFormRepository.java
@@ -0,0 +1,5 @@
+package com.ecommerce.loginsignup.ContactUs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactFormRepository extends JpaRepository<ContactForm, Long> {
+}
